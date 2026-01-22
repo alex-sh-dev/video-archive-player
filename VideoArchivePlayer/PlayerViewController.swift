dcf1889c857e7201e8627a411e62927b1ee7b499
alex <dev@alex-mb.local>
2026-01-22 16:14:03 +0500

    feat: added PlayerViewController

---
+++ VideoArchivePlayer/PlayerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerViewController.swift
+//  VideoArchivePlayer
+//
+//  Created by dev on 1/22/26.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
