1db01571cf5afdf9137cf019d67fbccbd7b8243d
alex <dev@alex-mb.local>
2026-01-22 16:00:18 +0500

    feat: SwiftUI -> Storyboard

---
+++ VideoArchivePlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoArchivePlayer
+//
+//  Created by dev on 1/22/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
