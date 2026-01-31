b3f153a2f537b7403aa6888186432c748acd365b
alex <dev@alex-mb.local>
2026-01-31 17:23:44 +0500

    feat: implemented VideoView

--- Player/Views/VideoView.swift
+++ Player/Views/VideoView.swift
@@ -8,5 +8,31 @@
 import UIKit
 
 class VideoView: UIView {
- 
+    var originalVideoSize: CGSize = CGSizeZero
+    private let expectedVideoView = "VLCOpenGLES2VideoView"
+    
+    final func calcScaleFactor() -> CGFloat {
+        let fs = self.frame.size
+        let ovs = self.originalVideoSize
+        let defScale = UIScreen.main.scale
+
+        if CGSizeEqualToSize(ovs, CGSizeZero) {
+            return defScale
+        }
+
+        if fs.height > ovs.height && fs.width > ovs.width {
+            let fh = ovs.height / fs.height
+            let fw = ovs.width / fs.width
+            return CGFloat.maximum(fh, fw) * defScale
+        }
+
+        return defScale
+    }
+
+    override func didAddSubview(_ subview: UIView) {
+        let strFromClass = String(describing: subview.self)
+        assert(strFromClass.contains(expectedVideoView), "Video view is not \(expectedVideoView)")
+        super.didAddSubview(subview)
+        subview.contentScaleFactor = calcScaleFactor()
+    }
 }
