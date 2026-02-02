10e5bf0316441964d3b655e7a8705daeb9e58b97
alex <dev@alex-mb.local>
2026-02-02 21:10:23 +0500

    feat: improved classes from Files.swift

--- Source/Files.swift
+++ Source/Files.swift
@@ -12,10 +12,15 @@ struct VideoFileInfo {
     var creationTime: UInt = 0
     /// Duration of video file
     var duration: UInt = 0
+    /// Size of video
+    var size: CGSize = CGSizeZero
     
-    init(creationTime: UInt, duration: UInt) {
+    init() {}
+    
+    init(creationTime: UInt, duration: UInt, size: CGSize) {
         self.creationTime = creationTime
         self.duration = duration
+        self.size = size
     }
     
     /// Returns the converted creation time (in seconds) from the interval [0, 86400) using the device's current time zone.
@@ -34,11 +39,33 @@ struct VideoFileInfo {
 }
 
 final class VideoFileList {
+    var count: UInt {
+        return UInt(_info.count)
+    }
+    
+    var first: (info: VideoFileInfo, path: String)? {
+        return self[0]
+    }
+    
+    var last: (info: VideoFileInfo, path: String)? {
+        if self.count > 0 {
+            return self[self.count - 1]
+        } else {
+            return nil
+        }
+    }
+    
+    var paths: [String] {
+        return _paths
+    }
+    
     private var _info = [VideoFileInfo]()
     private var _paths = [String]()
+
+    init() {}
     
-    func append(creationTime: UInt, duration: UInt, path: String) {
-        _info.append(VideoFileInfo(creationTime: creationTime, duration: duration))
+    func append(creationTime: UInt, duration: UInt, path: String, size: CGSize) {
+        _info.append(VideoFileInfo(creationTime: creationTime, duration: duration, size: size))
         _paths.append(path)
     }
     
@@ -47,21 +74,29 @@ final class VideoFileList {
         _paths.append(path)
     }
     
-    subscript(index: Int) -> (info: VideoFileInfo, path: String) {
+    private func validIndex(_ index: UInt) -> Bool {
+        return index >= 0 && index < self.count
+    }
+    
+    func remove(at index: UInt) {
+        if validIndex(index) {
+            _info.remove(at: Int(index))
+            _paths.remove(at: Int(index))
+        }
+    }
+    
+    subscript(index: UInt) -> (info: VideoFileInfo, path: String)? {
         get {
-            return (_info[index], _paths[index])
+            if !validIndex(index) {
+                return nil
+            }
+            return (_info[Int(index)], _paths[Int(index)])
         }
         set(newValue) {
-            _info[index] = newValue.info
-            _paths[index] = newValue.path
+            if validIndex(index) && newValue != nil {
+                _info[Int(index)] = newValue!.info
+                _paths[Int(index)] = newValue!.path
+            }
         }
-    }
-    
-    func count() -> Int {
-        return _info.count
-    }
-    
-    func paths() -> [String] {
-        return _paths
     }
 }
