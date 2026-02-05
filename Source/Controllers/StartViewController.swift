5877ce28de78f0afc40047997eed27e85667f7f5
alex <dev@alex-mb.local>
2026-02-05 14:50:07 +0500

    refactor: parse functional move to separated file and class VideoMetaDataParser

--- Source/Controllers/StartViewController.swift
+++ Source/Controllers/StartViewController.swift
@@ -7,41 +7,16 @@
 
 import UIKit
 
-private enum ParseError {
-    case parseFailed
-    case jsonLoadFailed
-    case urlEmpty
-    case urlInvalid
-    case jsonDataEmpty
-    
-    func desc() -> String {
-        switch (self) {
-        case .jsonLoadFailed:
-            return "JSON load failed!"
-        case .parseFailed:
-            return "JSON parse failed!"
-        case .urlEmpty:
-            return "Json url empty!"
-        case .urlInvalid:
-            return "Json url invalid!"
-        default:
-            return "Json data is empty or part is missing!"
-        }
-    }
-}
-
-class StartViewController: UIViewController {
+class StartViewController: UIViewController, VideoMetaDataParserDelegate {
     @IBOutlet weak var jsonUrlTextField: UITextField!
     @IBOutlet weak var startPlayerButton: UIButton!
     
-    private var videoFileList: VideoFileList = VideoFileList()
-    private var name = ""
-    private var date: Date = Date.now
-    private var videoSize: CGSize = .zero
+    private var _parser = VideoMetaDataParser()
+    private var _parseResult: VideoMetaDataParseResult?
     
     @IBAction func startPlayerTapped(_ sender: Any) {
         self.startPlayerButton.isEnabled = false
-        startParser()
+        _parser.start(for: self.jsonUrlTextField.text!)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -49,97 +24,39 @@ class StartViewController: UIViewController {
             return
         }
         
-        playerVC.videoFileList = videoFileList
-        playerVC.videoSize = videoSize
+        guard let result = _parseResult else {
+            return
+        }
         
-        let subtitle = date.formatted(date: .long, time: .omitted)
-        let titleView = TitleView(title: name, subtitle: subtitle)
+        playerVC.videoFileList = result.videoFileList
+        playerVC.videoSize = result.videoSize
+        
+        let subtitle = result.date.formatted(date: .long, time: .omitted)
+        let titleView = TitleView(title: result.name, subtitle: subtitle)
         playerVC.navigationItem.titleView = titleView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    private func startParser() {
-        if jsonUrlTextField.text!.isEmpty {
-            showAlert(for: .urlEmpty)
-            return
-        }
-        
-        guard let url = URL(string: self.jsonUrlTextField.text!) else {
-            showAlert(for: .urlInvalid)
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) {
-            [unowned self] (data, response, error) -> Void in
-            if error == nil && data != nil {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: Any]
-                    
-                    if let name = json["name"] as? String {
-                        self.name = name
-                    }
-                    
-                    if let dateStr = json["date"] as? String {
-                        let dateFormatter = DateFormatter()
-                        dateFormatter.dateFormat = "ddMMyyyy"
-                        self.date = dateFormatter.date(from: dateStr) ?? Date.distantPast
-                    }
-                   
-                    if let videoSizeStr = json["video_size"] as? String {
-                        let sizeList = videoSizeStr.split(separator: "x")
-                        if sizeList.count > 1 {
-                            let w = Int(sizeList[0]) ?? 0
-                            let h = Int(sizeList[1]) ?? 0
-                            self.videoSize = w > 0 && h > 0 ? CGSize(width: w, height: h) : .zero
-                        }
-                    }
-                    
-                    if let paths = json["paths"] as? Array<Dictionary<String, Any>> {
-                        for path in paths {
-                            guard let url = path["url"] as? String else {
-                                continue
-                            }
-                            
-                            let time = path["time"] as? UInt ?? 0
-                            
-                            guard let duration = path["duration"] as? UInt else {
-                                continue
-                            }
-                            
-                            self.videoFileList.append(time: time, duration: duration, path: url)
-                        }
-                    }
-                    DispatchQueue.main.async { self.parseFinished() }
-                } catch {
-                    DispatchQueue.main.async { self.parseFailed(.parseFailed) }
-                }
-            } else {
-                DispatchQueue.main.async { self.parseFailed(.jsonLoadFailed) }
-            }
-        }.resume()
-    }
-    
-    private func parseFinished() {
-        self.startPlayerButton.isEnabled = true
-        if self.videoFileList.count == 0 {
-            showAlert(for: .jsonDataEmpty)
-            return
-        }
-        self.performSegue(withIdentifier: PlayerViewController.className, sender: self)
-    }
-    
-    private func parseFailed(_ error: ParseError) {
-        self.startPlayerButton.isEnabled = true
-        self.showAlert(for: error)
+        _parser.delegate = self
     }
     
     private func showAlert(for error: ParseError) {
-        self.startPlayerButton.isEnabled = true
         let alert = UIAlertController(title: "Error", message: error.desc(), preferredStyle:.alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: VideoMetaDataParserDelegate
+    
+    func parseFinished(result: VideoMetaDataParseResult) {
+        self.startPlayerButton.isEnabled = true
+        _parseResult = result
+        self.performSegue(withIdentifier: PlayerViewController.className, sender: self)
+    }
+    
+    func parseFailed(err: ParseError) {
+        self.startPlayerButton.isEnabled = true
+        self.showAlert(for: err)
     }
 }
---
+++ Source/VideoMetaDataParser.swift
@@ -0,0 +1,121 @@
+//
+//  VideoMetaDataParser.swift
+//  VideoArchivePlayer
+//
+//  Created by dev on 2/5/26.
+//
+
+import Foundation
+
+enum ParseError {
+    case parseFailed
+    case jsonLoadFailed
+    case urlEmpty
+    case urlInvalid
+    case jsonDataEmpty
+    
+    func desc() -> String {
+        switch (self) {
+        case .jsonLoadFailed:
+            return "JSON load failed!"
+        case .parseFailed:
+            return "JSON parse failed!"
+        case .urlEmpty:
+            return "Json url empty!"
+        case .urlInvalid:
+            return "Json url invalid!"
+        default:
+            return "Json data is empty or part is missing!"
+        }
+    }
+}
+
+final class VideoMetaDataParseResult {
+    var name: String = ""
+    var date: Date = Date.now
+    var videoSize: CGSize = .zero
+    var videoFileList = VideoFileList()
+}
+
+protocol VideoMetaDataParserDelegate: AnyObject {
+    func parseFailed(err: ParseError)
+    func parseFinished(result: VideoMetaDataParseResult)
+}
+
+final class VideoMetaDataParser {
+    weak var delegate: VideoMetaDataParserDelegate?
+    
+    func start(for urlStr: String) {
+        if urlStr.isEmpty {
+            delegate?.parseFailed(err: .urlEmpty)
+            return
+        }
+        
+        guard let url = URL(string: urlStr) else {
+            delegate?.parseFailed(err: .urlInvalid)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) {
+            [unowned self] (data, response, error) -> Void in
+            let parseResult = VideoMetaDataParseResult()
+            if error == nil && data != nil {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: Any]
+                    
+                    if let name = json["name"] as? String {
+                        parseResult.name = name
+                    }
+                    
+                    if let dateStr = json["date"] as? String {
+                        let dateFormatter = DateFormatter()
+                        dateFormatter.dateFormat = "ddMMyyyy"
+                        parseResult.date = dateFormatter.date(from: dateStr) ?? Date.distantPast
+                    }
+                   
+                    if let videoSizeStr = json["video_size"] as? String {
+                        let sizeList = videoSizeStr.split(separator: "x")
+                        if sizeList.count > 1 {
+                            let w = Int(sizeList[0]) ?? 0
+                            let h = Int(sizeList[1]) ?? 0
+                            parseResult.videoSize = w > 0 && h > 0 ? CGSize(width: w, height: h) : .zero
+                        }
+                    }
+                    
+                    if let paths = json["paths"] as? Array<Dictionary<String, Any>> {
+                        for path in paths {
+                            guard let url = path["url"] as? String else {
+                                continue
+                            }
+                            
+                            let time = path["time"] as? UInt ?? 0
+                            
+                            guard let duration = path["duration"] as? UInt else {
+                                continue
+                            }
+                            
+                            parseResult.videoFileList.append(time: time, duration: duration, path: url)
+                        }
+                    }
+                    if parseResult.videoFileList.count > 0 {
+                        DispatchQueue.main.async {
+                            self.delegate?.parseFinished(result: parseResult)
+                        }
+                    } else {
+                        DispatchQueue.main.async {
+                            self.delegate?.parseFailed(err: .jsonDataEmpty)
+                        }
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        self.delegate?.parseFailed(err: .parseFailed)
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.delegate?.parseFailed(err: .jsonLoadFailed)
+                }
+            }
+        }.resume()
+    }
+}
